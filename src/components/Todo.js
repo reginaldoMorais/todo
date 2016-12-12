c0cf5b4ed5eab9cd407eebce08e23a31f717454c
Reginaldo Morais <reginaldo.morais@b2wdigital.com>
2016-12-12 15:12:05 -0200

    Ajustes nos componentes React

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,8 +1,17 @@
 import React from 'react';
 
 class Todo extends React.Component {
+  renderItem() {
+    const { todo } = this.props;
+    if(todo.get('isDone')) {
+      return <strike>{todo.get('text')}</strike>;
+    } else {
+      return <span>{todo.get('text')}</span>;
+    }
+  }
+
   render() {
-    return <h1>Hello Word! Haha</h1>;
+    return this.renderItem();
   }
 }
 
---
+++ src/components/TodoList.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import Todo from './Todo';
+
+class TodoList extends React.Component {
+  
+  onSubmit(event, addTodo) {
+    const input = event.target;
+    const text = input.value;
+    const isEnterKey = (event.which == 13);
+    const isLongEnough = text.length > 0;
+
+    if(isEnterKey && isLongEnough) {
+      input.value = '';
+      addTodo(text);
+    }
+  }
+
+  render() {
+    const { todos, addTodo, toggleTodo } = this.props;
+    console.log(todos);
+    
+    return (
+      <div className='todo'>
+        <input type='text' placeholder='Adicionar tarefas' onKeyDown={(event) => this.onSubmit(event,  addTodo)} />
+        <ul className='todo__list'>
+          {todos.map(todo => (
+            <li key={todo.get('id')} className='todo__item' onClick={(event) => toggleTodo(todo.get('id'))}>
+              <Todo todo={todo} />
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
