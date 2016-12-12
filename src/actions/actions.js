d5f0b37b8978a0943a73988a1db56f814c30bcc1
Reginaldo Morais <reginaldo.morais@b2wdigital.com>
2016-12-12 15:12:39 -0200

    Criação das action

--- src/actions/actions.js
+++ src/actions/actions.js
@@ -1,4 +1,17 @@
-export default toggleTodo(id) {
+const uid = () => Math.random().toString(34).slice(2);
+
+export const addTodo = (text) => {
+  return {
+    type: 'ADD_TODO',
+    payload: {
+      id: uid(),
+      isDone: false,
+      text: text
+    }
+  }
+}
+
+export const toggleTodo = (id) => {
   return {
     type: 'TOGGLE_TODO',
     payload: id
